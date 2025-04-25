89785b50ece56dfe54cb67edb74d376492d639bf
Emad Anwer <emadanwer.official@gmail.com>
2025-04-25 17:37:27 +0300

    fix rye issues

--- app/db/session.py
+++ app/db/session.py
@@ -5,7 +5,12 @@ from sqlalchemy.orm import sessionmaker
 from app.core.config import settings
 
 engine = create_engine(
-    settings.DATABASE_URL, connect_args={"check_same_thread": False} if settings.DATABASE_URL.startswith("sqlite") else {}
+    settings.DATABASE_URL,
+    connect_args=(
+        {"check_same_thread": False}
+        if settings.DATABASE_URL.startswith("sqlite")
+        else {}
+    ),
 )
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
@@ -17,4 +22,4 @@ def get_db():
     try:
         yield db
     finally:
-        db.close() 
+        db.close()
